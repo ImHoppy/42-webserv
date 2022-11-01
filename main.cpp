3dde450d4069182daf413597f2101429f0cb43d1
Marius BRAETS <mbraets@student.42.fr>
2022-11-01 18:26:05 +0100

    add html files and example config file

--- main.cpp
+++ main.cpp
@@ -2,12 +2,22 @@
 #include <map>
 
 #define hw "hi"
-
+class Config
+{
+	public:
+	int _port;
+		Config() : _port(1)
+		{};
+};
 int main(int ac, char **av)
 {
+
+	int i = 1;
+	std::vector<Config>();
+
+	delete [] a;
 	(void)ac;
 	(void)av;
-	int i = 1;
-	std::cout << hw << i << "\n";
+	std::cout << hw << "\n";
 }
 
