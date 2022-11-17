05f76f37f20117193b66a539477d21c9e3cb0974
cdefonte <cdefonte@student.42.fr>
2022-11-17 21:05:45 +0100

    test: add delete folder line to test delete method on folder with folder in it

--- mkdirDeleteTests.sh
+++ mkdirDeleteTests.sh
@@ -4,3 +4,5 @@ mkdir -p prout/cdefonte_vs/ok_for_delete
 touch prout/cdefonte_vs/ok_for_delete/hihi.txt
 touch prout/cdefonte_vs/ok_for_delete/lala.html
 touch prout/cdefonte_vs/ok_for_delete/uh.php
+mkdir -p prout/cdefonte_vs/ok_for_delete/indir
+touch prout/cdefonte_vs/ok_for_delete/indir/prout.txt
