d4e1a00395ffe69097ec096c4fd39ce814c8ed3e
cdefonte <cdefonte@student.42.fr>
2022-11-17 20:42:48 +0100

    add bash script to create dir and files quickly for curl DELETE folder test

---
+++ mkdirDeleteTests.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+mkdir -p prout/cdefonte_vs/ok_for_delete
+touch prout/cdefonte_vs/ok_for_delete/hihi.txt
+touch prout/cdefonte_vs/ok_for_delete/lala.html
+touch prout/cdefonte_vs/ok_for_delete/uh.php
