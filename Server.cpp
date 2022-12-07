cb1e66aa0145c704ac65ace7137e5499e7dcd781
cdefonte <cdefonte@student.42.fr>
2022-12-07 20:05:30 +0100

    fix seggfault when none ip is matched (cest que du coup ca match un 0.0.0.0)

--- Server.cpp
+++ Server.cpp
@@ -124,6 +124,15 @@ ServerConfig*	Server::getConfigForRequest(Request* rqst)
 		if (ip == it->getHost())
 			matchs.push_back(&(*it));
 	}
+	/* Si matchs is empty c'est qu'il y a forcement un 0.0.0.0, a retourner. */
+	if (matchs.empty())
+	{
+		for (std::vector<ServerConfig>::iterator it = _configs.begin(); it != _configs.end(); ++it)
+		{
+			if (it->getHost() == 0)
+				matchs.push_back(&(*it));
+		}
+	}
 	/* Dans la liste des match ip:port on check si l'un d'eux a un servername qui
 	correspond au Host header. Si aucun, on return le premier de la liste. */
 	for (std::list<ServerConfig*>::iterator it = matchs.begin(); it != matchs.end(); ++it)
